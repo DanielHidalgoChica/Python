536e918ee6140496361d3ad39c7be645cd946641
Daniel Hidalgo Chica <daniel5000.dhc@gmail.com>
2022-04-04 10:15:11 +0200

    Primer ejemplo Python

---
+++ ejemplo.py
@@ -0,0 +1,13 @@
+nombre = ""
+edad = 0
+
+nombre = input("Dime tu nombre ")
+edad = (int)(input("Dime tu edad "))
+
+print(nombre, " - ", edad)
+
+if (edad>10):
+    print(edad+5)
+else: 
+    print("Eres menor de diez años")
+
