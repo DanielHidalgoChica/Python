2ef9ba1021c56fbcf9222cf6d27aa8a9f6c1cc1b
Daniel Hidalgo Chica <daniel5000.dhc@gmail.com>
2022-04-29 13:07:39 +0200

    Cadenas de caracteres -10

--- Cadenas de caracteres/Ejercicio_10.py
+++ Cadenas de caracteres/Ejercicio_10.py
@@ -1,2 +1,18 @@
 #Introducir una cadena de caracteres e indicar si es un palíndromo. Una palabra 
 #palíndroma es aquella que se lee igual adelante que atrás
+
+cad_or = input("Introduzca la cadena que quiere comprobar: ")
+cad_in = []
+cad = []
+
+for i in cad_or:
+    cad.append(i)
+
+for i in range(0,len(cad)):
+    cad_in.append(cad[len(cad)-i-1])
+
+if (cad_in == cad):
+    print("Es un palíndromo")
+else:
+    print("No es un palíndromo")
+
